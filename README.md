7f30fef68b15e1233b20068a2d597ce7dcf3e3cf
Sandro Wong <saloiofun@gmail.com>
2017-12-25 20:16:36 -0800

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# nyTimesSearch
-NY Times Search
+# NY Times Search
+
+![demo](assets/images/demo.png)
+
+This is a New York Times Article Search
+
+Link: https://saloiofun.github.io/nyTimesSearch/
+
+## Getting Started
+
+* Type a search term in the input box.
+
+* Select from dropdown the number of Records to retrieve. 
+
+* Input the Start Year and End Year.
+
+## Tools used
+
+* HTML 5
+* CSS
+* JavaScript
+* Jquery
+* Bootstrap 3
+* New York Times API
--- assets/javascript/ny.js
+++ assets/javascript/ny.js
@@ -1,54 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $('#resultPanel').hide()
 
-	$("#resultPanel").hide();
+  $('#clearAll').on('click', function () {
+    $('#search-result').empty()
+    $('#resultPanel').hide()
+  })
 
-	$("#clearAll").on("click", function () {
-		$("#search-result").empty();
-		$("#resultPanel").hide();
-	});
+  $('#search').on('click', function (event) {
+    $('#resultPanel').show()
+    $('#search-result').empty()
 
-	$("#search").on("click", function (event) {
+    event.preventDefault()
 
-		$("#resultPanel").show();
-		$("#search-result").empty();
-		
-		event.preventDefault();
+    var input = $('#search-input').val().trim()
+    var limit = $('#search-limit').val()
+    var startYear = $('#search-startYear').val().trim()
+    var endYear = $('#search-endYear').val().trim()
 
-		var input = $("#search-input").val().trim();
-		var limit = $("#search-limit").val();
-		var startYear = $("#search-startYear").val().trim();
-		var endYear = $("#search-endYear").val().trim();
+    var apiKey = '363a9df561ad4ff38619a427376c4648'
+    var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json'
+    url += '?' + $.param({
+      'api-key': apiKey,
+      'q': input
+    })
+    parseInt(startYear) ? url += '&begin_date=' + startYear + '0101' : url
+    parseInt(endYear) ? url += '&end_date=' + endYear + '1231' : url
 
-		var apiKey = "363a9df561ad4ff38619a427376c4648";
-		var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
-		url += '?' + $.param({
-			'api-key': apiKey,
-			'q' : input
-		});
-		parseInt(startYear) ? url += "&begin_date=" + startYear + "0101" : url;
-		parseInt(endYear) ? url += "&end_date=" + endYear + "1231" : url;
+    $.ajax({
+      url: url,
+      method: 'GET'
+    }).done(function (result) {
+      var results = result.response.docs
 
-		$.ajax({
-			url: url,
-			method: 'GET',
-		}).done(function(result) {
+      for (var i = 0; i < limit; i++) {
+        var container = $('<div class="well">')
+        var title = $('<h3>').text(results[i].headline.main)
+        var pubDate = $('<p>').text(results[i].pub_date.slice(0, 10))
+        var sectionName = $('<p>').text(results[i].section_name)
+        var articleURL = $('<a>').attr('href', results[i].web_url).text(results[i].web_url)
 
-			var results = result.response.docs;
+        container.append(title)
+        container.append(pubDate)
+        container.append(sectionName)
+        container.append(articleURL)
 
-			for (var i = 0; i < limit; i++) {
-				var container = $("<div class=\"well\">");
-				var title = $("<h3>").text(results[i].headline.main);
-				var pubDate = $("<p>").text(results[i].pub_date.slice(0,10));
-				var sectionName = $("<p>").text(results[i].section_name);				
-				var articleURL = $("<a>").attr("href", results[i].web_url).text(results[i].web_url);
-
-				container.append(title);
-				container.append(pubDate);
-				container.append(sectionName);
-				container.append(articleURL);		
-
-				$("#search-result").append(container);
-			}
-		});
-	});
-});
+        $('#search-result').append(container)
+      }
+    })
+  })
+})
